52408dfd029d3d79f3b268b14f69c6fc764bd424
Anivice Ives <aniviceives@gmail.com>
2024-12-02 14:45:45 +0800

    Allow adding extra arguments to QEMU
    Signed-off-by: Anivice Ives <aniviceives@gmail.com>

--- cmake_modules/qemu-emulator.cmake
+++ cmake_modules/qemu-emulator.cmake
@@ -1,16 +1,23 @@
 cmake_minimum_required(VERSION 3.29)
 
 include(${CMAKE_CURRENT_LIST_DIR}/check_for_program_availability.cmake)
+include(cmake_modules/default_args.cmake)
+
 check_for_program_availability(QEMU_EXECUTABLE  qemu-system-i386)
 check_for_program_availability(GDB_EXECUTABLE   gdb)
 check_for_program_availability(QEMU_IMG_EXEC    qemu-img)
+
+default_args(QEMU_EMULATION_EXTRA_ARGS "")
+default_args(QEMU_DEBUG_EXTRA_ARGS     "")
 
 function(add_qemu_emulation_target
         TARGET_NAME
         BOOT_SECTOR_FILE_NAME)
     add_custom_target(${TARGET_NAME}
             DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${BOOT_SECTOR_FILE_NAME}
-            COMMAND ${QEMU_EXECUTABLE} -smp 1 -cpu 486 -drive format=raw,file=${CMAKE_CURRENT_BINARY_DIR}/${BOOT_SECTOR_FILE_NAME} -m 32M
+            COMMAND ${QEMU_EXECUTABLE} ${QEMU_EMULATION_EXTRA_ARGS} -smp 1 -cpu 486
+                                        -drive format=raw,file=${CMAKE_CURRENT_BINARY_DIR}/${BOOT_SECTOR_FILE_NAME}
+                                        -m 32M
             USES_TERMINAL
             COMMENT "Starting emulation for ${BOOT_SECTOR_FILE_NAME}"
     )
@@ -21,7 +28,10 @@ function(add_qemu_debug_target
         BOOT_SECTOR_FILE_NAME)
     add_custom_target(${TARGET_NAME}
             DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${BOOT_SECTOR_FILE_NAME}
-            COMMAND ${QEMU_EXECUTABLE} -smp 1 -cpu 486 -drive format=raw,file=${CMAKE_CURRENT_BINARY_DIR}/${BOOT_SECTOR_FILE_NAME} -m 32M -s -S -monitor stdio -d int,in_asm,cpu,exec -D qemu_debug_${TARGET_NAME}.log
+            COMMAND ${QEMU_EXECUTABLE} ${QEMU_DEBUG_EXTRA_ARGS} -smp 1 -cpu 486
+                                        -drive format=raw,file=${CMAKE_CURRENT_BINARY_DIR}/${BOOT_SECTOR_FILE_NAME}
+                                        -m 32M -s -S -monitor stdio -d int,in_asm,cpu,exec
+                                        -D qemu_debug_${TARGET_NAME}.log
             USES_TERMINAL
             COMMENT "Starting debug emulation for ${BOOT_SECTOR_FILE_NAME}. Emulation will start once gdb connects"
     )
