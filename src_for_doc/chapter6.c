82179d8b62c6e6f0153a221411079c9b0cba45c3
Anivice Ives <aniviceives@gmail.com>
2024-12-09 18:29:48 +0800

    polished a bit
    Signed-off-by: Anivice Ives <aniviceives@gmail.com>

--- src_for_doc/chapter6.c
+++ src_for_doc/chapter6.c
@@ -5,32 +5,45 @@ __asm__(
     "jmp __start                            \n\t"
     ".att_syntax prefix                     \n\t");
 
+#define __INLINE_ASM__(asm)                 \
+    __asm__(                                \
+        ".intel_syntax noprefix\n\t"        \
+        asm                                 \
+        "\n\t.att_syntax prefix\n\t")
+
+#define __VOLATILE_INLINE_ASM__(asm)        \
+__asm__ __volatile__(                       \
+        ".intel_syntax noprefix\n\t"        \
+        asm                                 \
+        "\n\t.att_syntax prefix\n\t")
+
+#define __STARTING_POINT__() __asm__ __volatile__(".intel_syntax noprefix\n\t__start:\n\t")
+#define __END_POINT__() __asm__("__halt_system: hlt\n\tjmp __halt_system\n\t");
+
 static void __start__ (void)
 {
-    __asm__ __volatile__(".intel_syntax noprefix    \n\t"
-        "__start:                                   \n\t"
-        "mov byte ptr [ds:0x00], 'S'     \n\t"
-        "mov byte ptr [ds:0x02], 'y'     \n\t"
-        "mov byte ptr [ds:0x04], 's'     \n\t"
-        "mov byte ptr [ds:0x06], 't'     \n\t"
-        "mov byte ptr [ds:0x08], 'e'     \n\t"
-        "mov byte ptr [ds:0x0A], 'm'     \n\t"
-        "mov byte ptr [ds:0x0C], ' '     \n\t"
-        "mov byte ptr [ds:0x0E], 'i'     \n\t"
-        "mov byte ptr [ds:0x10], 's'     \n\t"
-        "mov byte ptr [ds:0x12], ' '     \n\t"
-        "mov byte ptr [ds:0x14], 'i'     \n\t"
-        "mov byte ptr [ds:0x16], 'n'     \n\t"
-        "mov byte ptr [ds:0x18], ' '     \n\t"
-        "mov byte ptr [ds:0x1A], '3'     \n\t"
-        "mov byte ptr [ds:0x1C], '2'     \n\t"
-        "mov byte ptr [ds:0x1E], 'b'     \n\t"
-        "mov byte ptr [ds:0x20], 'i'     \n\t"
-        "mov byte ptr [ds:0x22], 't'     \n\t"
-      ".att_syntax prefix\n\t");
+    __STARTING_POINT__();
 
-    __asm__("hlt");
-    __asm__("jmp .");
+    __VOLATILE_INLINE_ASM__("mov byte ptr [ds:0x00], 'S'");
+    __VOLATILE_INLINE_ASM__("mov byte ptr [ds:0x02], 'y'");
+    __VOLATILE_INLINE_ASM__("mov byte ptr [ds:0x04], 's'");
+    __VOLATILE_INLINE_ASM__("mov byte ptr [ds:0x06], 't'");
+    __VOLATILE_INLINE_ASM__("mov byte ptr [ds:0x08], 'e'");
+    __VOLATILE_INLINE_ASM__("mov byte ptr [ds:0x0A], 'm'");
+    __VOLATILE_INLINE_ASM__("mov byte ptr [ds:0x0C], ' '");
+    __VOLATILE_INLINE_ASM__("mov byte ptr [ds:0x0E], 'i'");
+    __VOLATILE_INLINE_ASM__("mov byte ptr [ds:0x10], 's'");
+    __VOLATILE_INLINE_ASM__("mov byte ptr [ds:0x12], ' '");
+    __VOLATILE_INLINE_ASM__("mov byte ptr [ds:0x14], 'i'");
+    __VOLATILE_INLINE_ASM__("mov byte ptr [ds:0x16], 'n'");
+    __VOLATILE_INLINE_ASM__("mov byte ptr [ds:0x18], ' '");
+    __VOLATILE_INLINE_ASM__("mov byte ptr [ds:0x1A], '3'");
+    __VOLATILE_INLINE_ASM__("mov byte ptr [ds:0x1C], '2'");
+    __VOLATILE_INLINE_ASM__("mov byte ptr [ds:0x1E], 'b'");
+    __VOLATILE_INLINE_ASM__("mov byte ptr [ds:0x20], 'i'");
+    __VOLATILE_INLINE_ASM__("mov byte ptr [ds:0x22], 't'");
+
+    __END_POINT__();
 }
 
 __asm__("__global_end:\n\t");
